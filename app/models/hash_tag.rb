919b0f882c83b1c6d30fc920c4d2a79b4f84aed7
EC2 Default User <ec2-user@ip-172-31-42-124.ap-northeast-1.compute.internal>
2019-08-30 12:48:43 +0000

    Modified  for Heroku

--- app/models/hash_tag.rb
+++ app/models/hash_tag.rb
@@ -1,6 +1,6 @@
 class HashTag < ApplicationRecord
   has_many :post_hash_tags
   has_many :posts, through: :post_hash_tags
-  has_many :ecents, through: :eventhashes
+  has_many :events, through: :eventhashes
 
 end
